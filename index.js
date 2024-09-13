042936887bb4d98bb97f04805c8d860d9e4afb76
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-13 03:45:43 +0000

    ES Module  in node js & Path Module

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Sayak as I am Learning Express Js!");
+  res.sendFile(path.join(__dirname, "templates", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates", "about.html"));
 });
 
 app.listen(3000, () => {
-  console.log("Express server initialized");
+  console.log("Express server listening on port 3000");
 });
