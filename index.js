18ab3d8b8f14bdd8186a54e550d5a6dc185388e3
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-13 11:28:30 +0000

    Json response in the routes

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Route is /render.html
+app.use(express.static(path.join(__dirname, "public")));
+// Express Static Middleware
+
+let posts = [
+  { id: 1, name: "Sayak Ghosh" },
+  { id: 2, name: "Sagar Gaya" },
+  { id: 3, name: "Prince Kumar" },
+];
+
+// JSON response in Routes 
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "templates", "index.html"));
 });
